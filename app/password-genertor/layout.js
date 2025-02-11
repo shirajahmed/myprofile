af181a4c2e611c589bd6605da86ebd5b0e7b3e4b
shirajahmed <shirajad690@gmail.com>
2025-02-11 13:00:38 +0530

    meta data

---
+++ app/password-genertor/layout.js
@@ -0,0 +1,9 @@
+// app/password-generator/layout.js (or a parent layout file)
+export const metadata = {
+  title: "QR n Developer | Shiraj Ahmed",
+  description: "Best web Developer in silchar",
+};
+
+export default function Layout({ children }) {
+  return <div>{children}</div>;
+}
